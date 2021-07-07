f7cd1c95778bb4d2e4547bd75b97699f8eeb28a8
Dominic Rico Gómez <dominic.rico@sevdesk.de>
2021-07-07 12:00:48 +0200

    :rocket: deploy(fix): fix helm deployment

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const PORT = 50051
 const connectRetry = t => {
   let tries = t
 
-  return connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@sproud-cluster${process.env.NODE_ENV !== 'production' ? '-dev' : ''}${process.env.MONGO_HOST}/sproud${process.env.NODE_ENV === 'development' ? '-dev' : ''}?retryWrites=true&w=majority`, {
+  return connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_COLLECTION}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
